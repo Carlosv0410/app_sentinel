313b7884afc09155e6e0d6ee787dbd8bea74bfcd
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2021-11-08 14:01:36 -0500

    Update concesiones.py

--- concesiones.py
+++ concesiones.py
@@ -4,17 +4,17 @@ import matplotlib.patches as patches
 from PIL import Image
 
 def  imagen_concesiones():
-	im = Image.open('Zona de estudio/Zona_de_estudio3.png')
+	im = Image.open('Zona_estudio/Zona_de_estudio3.png')
 	return im
 
 def  seleccion_zona(zona, year):
 	if zona =='Zona 1 red':
 
-		img1=Image.open('Galeria concesiones/Zona1/2017.png')
-		img2=Image.open('Galeria concesiones/Zona1/2018.png')
-		img3=Image.open('Galeria concesiones/Zona1/2019.png')
-		img4=Image.open('Galeria concesiones/Zona1/2020.png')
-		img5=Image.open('Galeria concesiones/Zona1/2021.png')
+		img1=Image.open('Galeria_concesiones/Zona1/2017.png')
+		img2=Image.open('Galeria_concesiones/Zona1/2018.png')
+		img3=Image.open('Galeria_concesiones/Zona1/2019.png')
+		img4=Image.open('Galeria_concesiones/Zona1/2020.png')
+		img5=Image.open('Galeria_concesiones/Zona1/2021.png')
 
 		if year==2017:
 			fig=img1
@@ -29,11 +29,11 @@ def  seleccion_zona(zona, year):
 
 	elif zona =='Zona 2 yellow':
 
-		img1=Image.open('Galeria concesiones/Zona2/2017.png')
-		img2=Image.open('Galeria concesiones/Zona2/2018.png')
-		img3=Image.open('Galeria concesiones/Zona2/2019.png')
-		img4=Image.open('Galeria concesiones/Zona2/2020.png')
-		img5=Image.open('Galeria concesiones/Zona2/2021.png')
+		img1=Image.open('Galeria_concesiones/Zona2/2017.png')
+		img2=Image.open('Galeria_concesiones/Zona2/2018.png')
+		img3=Image.open('Galeria_concesiones/Zona2/2019.png')
+		img4=Image.open('Galeria_concesiones/Zona2/2020.png')
+		img5=Image.open('Galeria_concesiones/Zona2/2021.png')
 
 		if year==2017:
 			fig=img1
@@ -47,11 +47,11 @@ def  seleccion_zona(zona, year):
 			fig=img5
 	elif zona =='Zona 3 cian':
 
-		img1=Image.open('Galeria concesiones/Zona3/2017.png')
-		img2=Image.open('Galeria concesiones/Zona3/2018.png')
-		img3=Image.open('Galeria concesiones/Zona3/2019.png')
-		img4=Image.open('Galeria concesiones/Zona3/2020.png')
-		img5=Image.open('Galeria concesiones/Zona3/2021.png')
+		img1=Image.open('Galeria_concesiones/Zona3/2017.png')
+		img2=Image.open('Galeria_concesiones/Zona3/2018.png')
+		img3=Image.open('Galeria_concesiones/Zona3/2019.png')
+		img4=Image.open('Galeria_concesiones/Zona3/2020.png')
+		img5=Image.open('Galeria_concesiones/Zona3/2021.png')
 
 		if year==2017:
 			fig=img1
@@ -66,6 +66,6 @@ def  seleccion_zona(zona, year):
 
 	else:
 
-		fig = Image.open('Zona de estudio/Zona_de_estudio4.png')
+		fig = Image.open('Zona_estudio/Zona_de_estudio4.png')
 
 	return fig
