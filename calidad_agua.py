189b30f44575d60bb08d936dc0b4467539545fd9
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2021-11-07 22:55:04 -0500

    Update calidad_agua.py

--- calidad_agua.py
+++ calidad_agua.py
@@ -8,11 +8,11 @@ from PIL import Image
 def  seleccion_zona(zona, year):
 	if zona =='Zona 1 red':
 
-		img1=Image.open('Evaluacion_suelo\\Zona1\\2017\\2017.png')
-		img2=Image.open('Evaluacion_suelo\\Zona1\\2018\\2018.png')
-		img3=Image.open('Evaluacion_suelo\\Zona1\\2019\\2019.png')
-		img4=Image.open('Evaluacion_suelo\\Zona1\\2020\\2020.png')
-		img5=Image.open('Evaluacion_suelo\\Zona1\\2021\\2021.png')
+		img1=Image.open('Evaluacion_suelo/Zona1/2017/2017.png')
+		img2=Image.open('Evaluacion_suelo/Zona1/2018/2018.png')
+		img3=Image.open('Evaluacion_suelo/Zona1/2019/2019.png')
+		img4=Image.open('Evaluacion_suelo/Zona1/2020/2020.png')
+		img5=Image.open('Evaluacion_suelo/Zona1/2021/2021.png')
 
 		if year==2017:
 			fig, ax = plt.subplots(figsize = (18,15))
@@ -32,11 +32,11 @@ def  seleccion_zona(zona, year):
 
 	if zona =='Zona 2 yellow':
 
-		img1=Image.open('Evaluacion_suelo\\Zona2\\2017\\2017.png')
-		img2=Image.open('Evaluacion_suelo\\Zona2\\2018\\2018.png')
-		img3=Image.open('Evaluacion_suelo\\Zona2\\2019\\2019.png')
-		img4=Image.open('Evaluacion_suelo\\Zona2\\2020\\2020.png')
-		img5=Image.open('Evaluacion_suelo\\Zona2\\2021\\2021.png')
+		img1=Image.open('Evaluacion_suelo/Zona2/2017/2017.png')
+		img2=Image.open('Evaluacion_suelo/Zona2/2018/2018.png')
+		img3=Image.open('Evaluacion_suelo/Zona2/2019/2019.png')
+		img4=Image.open('Evaluacion_suelo/Zona2/2020/2020.png')
+		img5=Image.open('Evaluacion_suelo/Zona2/2021/2021.png')
 
 
 		if year==2017:
@@ -57,11 +57,11 @@ def  seleccion_zona(zona, year):
 
 	if zona =='Zona 3 cian':
 
-		img1=Image.open('Evaluacion_suelo\\Zona3\\2017\\2017.png')
-		img2=Image.open('Evaluacion_suelo\\Zona3\\2018\\2018.png')
-		img3=Image.open('Evaluacion_suelo\\Zona3\\2019\\2019.png')
-		img4=Image.open('Evaluacion_suelo\\Zona3\\2020\\2020.png')
-		img5=Image.open('Evaluacion_suelo\\Zona3\\2021\\2021.png')
+		img1=Image.open('Evaluacion_suelo/Zona3/2017/2017.png')
+		img2=Image.open('Evaluacion_suelo/Zona3/2018/2018.png')
+		img3=Image.open('Evaluacion_suelo/Zona3/2019/2019.png')
+		img4=Image.open('Evaluacion_suelo/Zona3/2020/2020.png')
+		img5=Image.open('Evaluacion_suelo/Zona3/2021/2021.png')
 
 		if year==2017:
 			fig, ax = plt.subplots(figsize = (18,15))
@@ -79,6 +79,7 @@ def  seleccion_zona(zona, year):
 			fig, ax = plt.subplots(figsize = (18,15))
 			ax.imshow(img5)
 	return fig
+
 
 
 
