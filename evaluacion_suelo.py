aded67858c3bce4db768c59989560e5ee99f90e9
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2021-11-09 23:16:08 -0500

    Update evaluacion_suelo.py

--- evaluacion_suelo.py
+++ evaluacion_suelo.py
@@ -10,35 +10,40 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2017/B04.tiff",
 		            	"Evaluacion_suelo/Zona1/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2017/B11.tiff",
-		            	"Evaluacion_suelo/Zona1/2017/B12.tiff"]
+		            	"Evaluacion_suelo/Zona1/2017/B12.tiff",
+				    "Evaluacion_suelo/Zona1/2017/B09.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona1/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2018/B03.tiff",
 		            	"Evaluacion_suelo/Zona1/2018/B04.tiff",
 		            	"Evaluacion_suelo/Zona1/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2018/B11.tiff",
-		            	"Evaluacion_suelo/Zona1/2018/B12.tiff"]
+		            	"Evaluacion_suelo/Zona1/2018/B12.tiff",
+				   "Evaluacion_suelo/Zona1/2018/B09.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona1/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2019/B03.tiff",
 		            	"Evaluacion_suelo/Zona1/2019/B04.tiff",
 		            	"Evaluacion_suelo/Zona1/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2019/B11.tiff",
-		            	"Evaluacion_suelo/Zona1/2019/B12.tiff"]
+		            	"Evaluacion_suelo/Zona1/2019/B12.tiff",
+				   "Evaluacion_suelo/Zona1/2019/B09.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona1/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2020/B03.tiff",
 		            	"Evaluacion_suelo/Zona1/2020/B04.tiff",
 		            	"Evaluacion_suelo/Zona1/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2020/B11.tiff",
-		            	"Evaluacion_suelo/Zona1/2020/B12.tiff"]
+		            	"Evaluacion_suelo/Zona1/2020/B12.tiff",
+				   "Evaluacion_suelo/Zona1/2020/B09.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona1/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2021/B03.tiff",
 		            	"Evaluacion_suelo/Zona1/2021/B04.tiff",
 		            	"Evaluacion_suelo/Zona1/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2021/B11.tiff",
-		            	"Evaluacion_suelo/Zona1/2021/B12.tiff"]
+		            	"Evaluacion_suelo/Zona1/2021/B12.tiff",
+				   "Evaluacion_suelo/Zona1/2021/B09.tiff"]
 
 
 
@@ -50,35 +55,40 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2017/B04.tiff",
 		            	"Evaluacion_suelo/Zona2/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2017/B11.tiff",
-		            	"Evaluacion_suelo/Zona2/2017/B12.tiff"]
+		            	"Evaluacion_suelo/Zona2/2017/B12.tiff",
+				   "Evaluacion_suelo/Zona2/2017/B09.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona2/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2018/B03.tiff",
 		            	"Evaluacion_suelo/Zona2/2018/B04.tiff",
 		            	"Evaluacion_suelo/Zona2/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2018/B11.tiff",
-		            	"Evaluacion_suelo/Zona2/2018/B12.tiff"]
+		            	"Evaluacion_suelo/Zona2/2018/B12.tiff",
+				   "Evaluacion_suelo/Zona2/2018/B09.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona2/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2019/B03.tiff",
 		            	"Evaluacion_suelo/Zona2/2019/B04.tiff",
 		            	"Evaluacion_suelo/Zona2/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2019/B11.tiff",
-		            	"Evaluacion_suelo/Zona2/2019/B12.tiff"]
+		            	"Evaluacion_suelo/Zona2/2019/B12.tiff",
+				   "Evaluacion_suelo/Zona2/2019/B09.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona2/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2020/B03.tiff",
 		            	"Evaluacion_suelo/Zona2/2020/B04.tiff",
 		            	"Evaluacion_suelo/Zona2/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2020/B11.tiff",
-		            	"Evaluacion_suelo/Zona2/2020/B12.tiff"]
+		            	"Evaluacion_suelo/Zona2/2020/B12.tiff",
+				   "Evaluacion_suelo/Zona2/2020/B09.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona2/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2021/B03.tiff",
 		            	"Evaluacion_suelo/Zona2/2021/B04.tiff",
 		            	"Evaluacion_suelo/Zona2/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2021/B11.tiff",
-		            	"Evaluacion_suelo/Zona2/2021/B12.tiff"]
+		            	"Evaluacion_suelo/Zona2/2021/B12.tiff",
+				   "Evaluacion_suelo/Zona2/2021/B09.tiff"]
 
 
 	if zona== 'Zona 3 cian':
@@ -89,35 +99,40 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2017/B04.tiff",
 		            	"Evaluacion_suelo/Zona3/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2017/B11.tiff",
-		            	"Evaluacion_suelo/Zona3/2017/B12.tiff"]
+		            	"Evaluacion_suelo/Zona3/2017/B12.tiff",
+				   "Evaluacion_suelo/Zona3/2017/B09.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona3/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2018/B03.tiff",
 		            	"Evaluacion_suelo/Zona3/2018/B04.tiff",
 		            	"Evaluacion_suelo/Zona3/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2018/B11.tiff",
-		            	"Evaluacion_suelo/Zona3/2018/B12.tiff"]
+		            	"Evaluacion_suelo/Zona3/2018/B12.tiff",
+				   "Evaluacion_suelo/Zona3/2018/B09.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona3/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2019/B03.tiff",
 		            	"Evaluacion_suelo/Zona3/2019/B04.tiff",
 		            	"Evaluacion_suelo/Zona3/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2019/B11.tiff",
-		            	"Evaluacion_suelo/Zona3/2019/B12.tiff"]
+		            	"Evaluacion_suelo/Zona3/2019/B12.tiff",
+				   "Evaluacion_suelo/Zona3/2019/B09.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona3/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2020/B03.tiff",
 		            	"Evaluacion_suelo/Zona3/2020/B04.tiff",
 		            	"Evaluacion_suelo/Zona3/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2020/B11.tiff",
-		            	"Evaluacion_suelo/Zona3/2020/B12.tiff"]
+		            	"Evaluacion_suelo/Zona3/2020/B12.tiff",
+				   "Evaluacion_suelo/Zona3/2020/B09.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona3/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2021/B03.tiff",
 		            	"Evaluacion_suelo/Zona3/2021/B04.tiff",
 		            	"Evaluacion_suelo/Zona3/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2021/B11.tiff",
-		            	"Evaluacion_suelo/Zona3/2021/B12.tiff"]
+		            	"Evaluacion_suelo/Zona3/2021/B12.tiff",
+				   "Evaluacion_suelo/Zona3/2021/B09.tiff"]
 
 	return s2_bands
 
