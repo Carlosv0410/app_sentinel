adcc565e2fb3708b9f03f3e9a733ce4a6567136f
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2021-11-12 22:56:52 -0500

    Update app_sentinel_v01.py

--- app_sentinel_v01.py
+++ app_sentinel_v01.py
@@ -680,7 +680,7 @@ if option == '⛰ Evaluación de cobertura vegetal':
 
 		
 	with st.expander('Indices espectrales para Sentinel'):
-		indice_ndvi_ = (band08.astype(float)-band04.astype(float) )/ (band08.astype(float)+band04.astype(float))
+		indice_ndvi = (band08.astype(float)-band04.astype(float) )/ (band08.astype(float)+band04.astype(float))
 		indice_ndwi =  (band08.astype(float)-(band05.astype(float)*1))
 		indice_gndvi = (band08.astype(float)-band03.astype(float))/(band08.astype(float)+band03.astype(float))
 		indice_tgi = band03.astype(float)-0.39*band04.astype(float)-0.61*band02.astype(float)
