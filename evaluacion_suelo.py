8b7c543589f41f591cd00f416f89f54faef25509
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2021-11-12 20:09:27 -0500

    Update evaluacion_suelo.py

--- evaluacion_suelo.py
+++ evaluacion_suelo.py
@@ -11,7 +11,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2017/B11.tiff",
 		            	"Evaluacion_suelo/Zona1/2017/B12.tiff",
-				    "Evaluacion_suelo/Zona1/2017/B09.tiff"]
+				    "Evaluacion_suelo/Zona1/2017/B09.tiff",
+				    "Evaluacion_suelo/Zona1/2017/B05.tiff",
+				    "Evaluacion_suelo/Zona1/2017/B06.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona1/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2018/B03.tiff",
@@ -19,7 +21,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2018/B11.tiff",
 		            	"Evaluacion_suelo/Zona1/2018/B12.tiff",
-				   "Evaluacion_suelo/Zona1/2018/B09.tiff"]
+				   "Evaluacion_suelo/Zona1/2018/B09.tiff",
+				   "Evaluacion_suelo/Zona1/2018/B05.tiff",
+				   "Evaluacion_suelo/Zona1/2018/B06.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona1/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2019/B03.tiff",
@@ -27,7 +31,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2019/B11.tiff",
 		            	"Evaluacion_suelo/Zona1/2019/B12.tiff",
-				   "Evaluacion_suelo/Zona1/2019/B09.tiff"]
+				   "Evaluacion_suelo/Zona1/2019/B09.tiff",
+				   "Evaluacion_suelo/Zona1/2019/B05.tiff",
+				   "Evaluacion_suelo/Zona1/2019/B06.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona1/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2020/B03.tiff",
@@ -35,7 +41,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2020/B11.tiff",
 		            	"Evaluacion_suelo/Zona1/2020/B12.tiff",
-				   "Evaluacion_suelo/Zona1/2020/B09.tiff"]
+				   "Evaluacion_suelo/Zona1/2020/B09.tiff",
+				   "Evaluacion_suelo/Zona1/2020/B05.tiff",
+				   "Evaluacion_suelo/Zona1/2020/B06.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona1/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona1/2021/B03.tiff",
@@ -43,7 +51,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona1/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona1/2021/B11.tiff",
 		            	"Evaluacion_suelo/Zona1/2021/B12.tiff",
-				   "Evaluacion_suelo/Zona1/2021/B09.tiff"]
+				   "Evaluacion_suelo/Zona1/2021/B09.tiff",
+				   "Evaluacion_suelo/Zona1/2021/B05.tiff",
+				   "Evaluacion_suelo/Zona1/2021/B06.tiff"]
 
 
 
@@ -56,7 +66,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2017/B11.tiff",
 		            	"Evaluacion_suelo/Zona2/2017/B12.tiff",
-				   "Evaluacion_suelo/Zona2/2017/B09.tiff"]
+				   "Evaluacion_suelo/Zona2/2017/B09.tiff",
+				   "Evaluacion_suelo/Zona2/2017/B05.tiff",
+				   "Evaluacion_suelo/Zona2/2017/B06.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona2/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2018/B03.tiff",
@@ -64,7 +76,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2018/B11.tiff",
 		            	"Evaluacion_suelo/Zona2/2018/B12.tiff",
-				   "Evaluacion_suelo/Zona2/2018/B09.tiff"]
+				   "Evaluacion_suelo/Zona2/2018/B09.tiff",
+				   "Evaluacion_suelo/Zona2/2018/B05.tiff",
+				   "Evaluacion_suelo/Zona2/2018/B06.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona2/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2019/B03.tiff",
@@ -72,7 +86,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2019/B11.tiff",
 		            	"Evaluacion_suelo/Zona2/2019/B12.tiff",
-				   "Evaluacion_suelo/Zona2/2019/B09.tiff"]
+				   "Evaluacion_suelo/Zona2/2019/B09.tiff",
+				   "Evaluacion_suelo/Zona2/2019/B05.tiff",
+				   "Evaluacion_suelo/Zona2/2019/B06.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona2/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2020/B03.tiff",
@@ -80,7 +96,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2020/B11.tiff",
 		            	"Evaluacion_suelo/Zona2/2020/B12.tiff",
-				   "Evaluacion_suelo/Zona2/2020/B09.tiff"]
+				   "Evaluacion_suelo/Zona2/2020/B09.tiff",
+				   "Evaluacion_suelo/Zona2/2020/B05.tiff",
+				   "Evaluacion_suelo/Zona2/2020/B06.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona2/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona2/2021/B03.tiff",
@@ -88,7 +106,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona2/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona2/2021/B11.tiff",
 		            	"Evaluacion_suelo/Zona2/2021/B12.tiff",
-				   "Evaluacion_suelo/Zona2/2021/B09.tiff"]
+				   "Evaluacion_suelo/Zona2/2021/B09.tiff",
+				   "Evaluacion_suelo/Zona2/2021/B05.tiff",
+				   "Evaluacion_suelo/Zona2/2021/B06.tiff"]
 
 
 	if zona== 'Zona 3 cian':
@@ -100,7 +120,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2017/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2017/B11.tiff",
 		            	"Evaluacion_suelo/Zona3/2017/B12.tiff",
-				   "Evaluacion_suelo/Zona3/2017/B09.tiff"]
+				   "Evaluacion_suelo/Zona3/2017/B09.tiff",
+				   "Evaluacion_suelo/Zona3/2017/B05.tiff",
+				   "Evaluacion_suelo/Zona3/2017/B06.tiff"]
 		if year ==2018:
 			s2_bands = ["Evaluacion_suelo/Zona3/2018/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2018/B03.tiff",
@@ -108,7 +130,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2018/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2018/B11.tiff",
 		            	"Evaluacion_suelo/Zona3/2018/B12.tiff",
-				   "Evaluacion_suelo/Zona3/2018/B09.tiff"]
+				   "Evaluacion_suelo/Zona3/2018/B09.tiff",
+				   "Evaluacion_suelo/Zona3/2018/B05.tiff",
+				   "Evaluacion_suelo/Zona3/2018/B06.tiff"]
 		if year ==2019:
 			s2_bands = ["Evaluacion_suelo/Zona3/2019/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2019/B03.tiff",
@@ -116,7 +140,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2019/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2019/B11.tiff",
 		            	"Evaluacion_suelo/Zona3/2019/B12.tiff",
-				   "Evaluacion_suelo/Zona3/2019/B09.tiff"]
+				   "Evaluacion_suelo/Zona3/2019/B09.tiff",
+				   "Evaluacion_suelo/Zona3/2019/B05.tiff",
+				   "Evaluacion_suelo/Zona3/2019/B06.tiff"]
 		if year ==2020:
 			s2_bands = ["Evaluacion_suelo/Zona3/2020/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2020/B03.tiff",
@@ -124,7 +150,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2020/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2020/B11.tiff",
 		            	"Evaluacion_suelo/Zona3/2020/B12.tiff",
-				   "Evaluacion_suelo/Zona3/2020/B09.tiff"]
+				   "Evaluacion_suelo/Zona3/2020/B09.tiff",
+				   "Evaluacion_suelo/Zona3/2020/B05.tiff",
+				   "Evaluacion_suelo/Zona3/2020/B06.tiff"]
 		if year ==2021:
 			s2_bands = ["Evaluacion_suelo/Zona3/2021/B02.tiff",
 	        		    "Evaluacion_suelo/Zona3/2021/B03.tiff",
@@ -132,7 +160,9 @@ def bandas(zona, year):
 		            	"Evaluacion_suelo/Zona3/2021/B08.tiff",
 		            	"Evaluacion_suelo/Zona3/2021/B11.tiff",
 		            	"Evaluacion_suelo/Zona3/2021/B12.tiff",
-				   "Evaluacion_suelo/Zona3/2021/B09.tiff"]
+				   "Evaluacion_suelo/Zona3/2021/B09.tiff",
+				   "Evaluacion_suelo/Zona3/2021/B05.tiff",
+				   "Evaluacion_suelo/Zona3/2021/B06.tiff"]
 
 	return s2_bands
 
